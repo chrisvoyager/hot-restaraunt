0c76868739195309cfe79ec1c471d9727ec1deba
Voyager <chriscoonwilliam@gmail.com>
2019-12-14 11:40:31 -0800

    added serverjs contents

--- java.js
+++ java.js
@@ -9,6 +9,19 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var tables = [];
+
+//create new reservations
+app.post("/api/tables", function(req, res) {
+  var newTable = req.body;
+
+  console.log(newTable);
+
+  tables.push(newTable);
+
+  res.json(newTable);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
